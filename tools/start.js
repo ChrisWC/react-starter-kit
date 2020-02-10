9e3f5731c55f86b36092a68ae2b6183d36bce842
C. W. Catton <christopherwcatton@gmail.com>
2020-02-10 22:40:28 +0100

    removing logs from compilation promise to reduce complexity

--- tools/start.js
+++ tools/start.js
@@ -34,24 +34,14 @@ function createCompilationPromise(name, compiler, config) {
     let timeStart = new Date();
     compiler.hooks.compile.tap(name, () => {
       timeStart = new Date();
-      console.info(`[${format(timeStart)}] Compiling '${name}'...`);
     });
 
     compiler.hooks.done.tap(name, stats => {
-      console.info(stats.toString(config.stats));
       const timeEnd = new Date();
       const time = timeEnd.getTime() - timeStart.getTime();
       if (stats.hasErrors()) {
-        console.info(
-          `[${format(timeEnd)}] Failed to compile '${name}' after ${time} ms`,
-        );
         reject(new Error('Compilation failed!'));
       } else {
-        console.info(
-          `[${format(
-            timeEnd,
-          )}] Finished '${name}' compilation after ${time} ms`,
-        );
         resolve(stats);
       }
     });
@@ -112,13 +102,11 @@ async function start(port, options) {
   );
   const clientPromise = createCompilationPromise(
     'client',
-    clientCompiler,
-    clientConfig,
+    clientCompiler
   );
   const serverPromise = createCompilationPromise(
     'server',
-    serverCompiler,
-    serverConfig,
+    serverCompiler
   );
 
   // https://github.com/webpack/webpack-dev-middleware
